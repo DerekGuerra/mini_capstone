7f87909dbec9411cd2f2f5d716a450792a97e334
“DerekE” <d12guerra@gmail.comm>
2019-03-05 20:37:48 -0600

    added is_discounted and tax method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    if price < 10 
+      p "Item is on clearance"
+    else
+      p "Item is not on clearance"
+    end
+  end
+
+  def tax
+    product_taxed = price * 1.09
+    p "Total price with tax is #{product_taxed}"
+  end
 end
--- app/views/api/products/index.json.jbuilder
+++ app/views/api/products/index.json.jbuilder
@@ -2,6 +2,8 @@ json.array! @products.each do |product|
   json.id product.id
   json.name product.name 
   json.price product.price
+  json.clearance? product.is_discounted?
+  json.total_price product.tax
   json.description product.description
   json.image_url product.image_url
-end
+end   
