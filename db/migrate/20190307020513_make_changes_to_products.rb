e2ffddd7531e2a101b85cf6b8f0cfa6a729a3823
“DerekE” <d12guerra@gmail.comm>
2019-03-11 20:24:07 -0500

    added supplier model and migrartion

--- db/migrate/20190307020513_make_changes_to_products.rb
+++ db/migrate/20190307020513_make_changes_to_products.rb
@@ -2,6 +2,7 @@ class MakeChangesToProducts < ActiveRecord::Migration[5.2]
   def change
     change_column :products, :description, :text
     change_column :products, :price, :decimal, precision: 6, scale: 2
-    add_column  :products, :in_stock, :string 
+    add_column  :products, :in_stock, :string
+    add_column :products, :supplier_id, :integer 
   end
 end
