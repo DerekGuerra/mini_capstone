94c9c543395831020ab8d7dd579292f6884d1e5a
“DerekE” <d12guerra@gmail.comm>
2019-03-21 11:56:39 -0500

    adds Order action for CartedOrder Model

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,15 +1,50 @@
 class Api::OrdersController < ApplicationController
   
   def create
-    product = Product.find_by(id: params[:product_id])
-    calculated_subtotal = params[:quantity].to_i * product.price
+    # subtotal = quantity * price
+    # product = Product.find_by(id: params[:product_id])
+    # calculated_subtotal = params[:quantity].to_i * product.price
+    # tax_rate = 0.09
+    # calculated_tax = calculated_subtotal * tax_rate
+    # calculated_total = calculated_subtotal + calculated_tax
+
+    # @order = Order.new(
+    #     user_id: current_user.id,
+    #     product_id: params[:product_id],
+    #     quantity: params[:quantity],
+    #     subtotal: calculated_subtotal,
+    #     tax: calculated_tax,
+    #     total: calculated_total)
+
+    # @order.save  
+    # render 'show.json.jbuilder'
+
+    @carted_products = current_user.carted_products
+    @carted_products = @carted_products.where(status: "carted")
+
+    calculated_subtotal = 0
+    @carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
+
     tax_rate = 0.09
     calculated_tax = calculated_subtotal * tax_rate
     calculated_total = calculated_subtotal + calculated_tax
 
-    @new_order = Order.new(user_id: current_user.id, product_id: params[:product_id], quantity: params[:quantity], subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
+    @order = Order.new(
+        user_id: current_user.id,
+        subtotal: calculated_subtotal, 
+        tax: calculated_tax, 
+        total: calculated_total, 
+        )
 
-    @new_order.save
+    @order.save
+
+    @carted_products.each do |carted_product|
+      carted_product.order_id = @order_id
+      carted_product.status = "purchased"
+      carted_product.save
+    end
     render 'show.json.jbuilder'
   end
 end
--- app/views/api/orders/show.json.jbuilder
+++ app/views/api/orders/show.json.jbuilder
@@ -1,7 +1,5 @@
-json.id @new_order.id
-json.user_id @new_order.user_id
-json.product_id @new_order.product_id
-json.quantity @new_order.quantity
-json.subtotal @new_order.subtotal
-json.tax @new_order.tax
-json.total @new_order.total
+json.id @order.id
+json.user_id @order.user_id
+json.subtotal @order.subtotal
+json.tax @order.tax
+json.total @order.total
